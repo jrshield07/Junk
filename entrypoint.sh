18687786d35ce1f8e01e5bb425ddc042fb8b27aa
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2021-06-27 20:50:32 +0300

    Remove unnecessary files

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "installing access-manager"
-chmod 750 /forgerock/openam/security/keystores/*
-/usr/local/tomcat/bin/catalina.sh run & (sleep 50 && /data/setup.sh)
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-whoami
-sudo chmod 750 /forgerock/openam/security/keystores/*
-/data/amster/amster /data/setup.amster
